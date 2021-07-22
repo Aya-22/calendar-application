c2ca8d127e5e3a06675972bb4771f39288362e1f
Jennifer Ayala <j.ayala0695@gmail.com>
2021-07-21 23:21:27 -0700

    updated html

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-var today = moment("07/20/21", "MM-DD-YY").format("dddd, MMMM Do YYYY");
+// date was added using moment
+var today = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(today);
+
+// 
+
+// var timeOfDay = moment().hour()
+// $('.container').text(timeOfDay)
+
+var inputEvent = $('#textArea');
+var saveEvent = $('.saveBtn');
+var save = true;
+
+// TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+
+function listedEvent (event){
+event.preventDefault(); //prevent default refreshing
+inputEvent.text('');
+
+if (save)
+console.log(inputEvent);
+//   shoppingListEl.append('text);
+}
+
+
+
+// // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+saveEvent.on('click', listedEvent);
+
