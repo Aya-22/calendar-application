a8f6248d32199dd4ce68ecaabee30f891dbd6f22
Jennifer Ayala <j.ayala0695@gmail.com>
2021-07-25 21:48:10 -0700

    updated functions

--- script.js
+++ script.js
@@ -17,20 +17,32 @@ function eventEntered (event) {
 event.preventDefault(); //prevent default refreshing
 
 // variable creates value in textbox in html
-var saved = inputEvent.val().trim();
+var saved = inputEvent.val();
 
 // if event saved then a message will appear with message below
 if (saved) {
- console.log('Your event has been saved')   
+ console.log('Your event has been saved')  
 }
 
+hourInput()
+
 }
-// function hourInput () {
+function hourInput () {
 // local storage
-// }
+var event = localStorage.getItem('Enter');
+
+if (!event) {
+    return;
+}
+
+inputEvent.textContent = event;
+}
+
 
 // function colorCode () {
 // past, present, future color change on schedule
+// if (hourEl === 09:00)
+
 
 // }
 
